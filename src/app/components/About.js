70fa290e865e23ff3cd205ee6cfaf5fd91358e08
tiagoinaba <tiago.inaba@gmail.com>
2023-06-21 16:45:47 -0300

    updated bio

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -15,6 +15,9 @@ import react from '../../assets/img/logos/react.png'
 import next from '../../assets/img/logos/next.png'
 import java from '../../assets/img/logos/java.png'
 import figma from '../../assets/img/logos/figma.png'
+import tailwind from '../../assets/img/logos/tailwind.svg'
+import spring from '../../assets/img/logos/spring.svg'
+import flutter from '../../assets/img/logos/flutter.svg'
 import { LanguageContextConsumer } from '../context/langContext'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
@@ -29,6 +32,8 @@ export default function About() {
             <br />
             Building on that, towards the end of 2022 - when I decided to work in this area - I learned a little bit of Spring but quickly saw it wasn’t for me. So I switched to front-end and that’s where I really found myself.<br />
             <br />
+            Of course I went back and picked Spring back up, which I used on a Flutter app I made for employee management that connects to a SolrCloud server. The source code for it can be seen <a href="https://github.com/tiagoinaba/cruddemo" target='_blank'>here</a>.<br />
+            <br />
             I feel particularly very passionate about good looking interfaces and CSS animations.<br />
             <br />
             I am also a guitarist who unluckily had to fill in as a bassist.<br />
@@ -42,6 +47,8 @@ export default function About() {
             Eu comecei com Java por volta dos 13 anos de idade, porque queria aprender a fazer plugins de Minecraft.<br />
             <br />
             Tendo isso como base, no final de 2022 - quando decidi trabalhar nessa área - comecei a aprender Spring, mas logo vi que não era para mim. Então decidi mudar meu foco para front-end, e foi aí que realmente encontrei meu caminho.<br />
+            <br />
+            É claro que eventualmente retornei ao meu aprendizado em Spring, tanto que o utilizei como o back-end de um aplicativo Flutter para gestão de funcionários que se conecta a um servidor SolrCloud. O código deste projeto pode ser encontrado <a href="https://github.com/tiagoinaba/cruddemo" target='_blank'>aqui</a>.<br />
             <br />
             Eu sou particularmente apaixonado por interfaces bonitas e animações de CSS.<br />
             <br />
@@ -88,6 +95,12 @@ export default function About() {
                                         alt="Sass"
                                     />
                                     <Image
+                                        src={tailwind}
+                                        width={102}
+                                        height={102}
+                                        alt="Tailwind"
+                                    />
+                                    <Image
                                         src={react}
                                         width={102}
                                         height={102}
@@ -104,6 +117,18 @@ export default function About() {
                                         width={102}
                                         height={102}
                                         alt="Java"
+                                    />
+                                    <Image
+                                        src={spring}
+                                        width={102}
+                                        height={102}
+                                        alt="Spring"
+                                    />
+                                    <Image
+                                        src={flutter}
+                                        width={102}
+                                        height={102}
+                                        alt="Flutter"
                                     />
                                     <Image
                                         src={figma}
